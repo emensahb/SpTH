4d502f41a1aed01b38a7c653fb9e85bcabb29cc5
Efua Mensah-Brown <esparrow@Efuas-Air.attlocal.net>
2022-09-06 11:06:12 -0500

    Sp TH initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import jsonData from './data.json'
 
+// console.log(data)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let Tag="" 
+  const data = JSON.parse(JSON.stringify(jsonData))
+  const [formData, setFormData] = React.useState({})
+
+
+  function handleSubmit(event) {
+    console.log(event)
+    event.preventDefault()
+    
+  }
+
+
+  function handleChange(event) {
+      setFormData(prevFormData => {
+          return {
+              ...prevFormData,
+              [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value
+        }
+      }
+      
+      )
+     
+    }
+
+  
+    function needsParentalConsent(){
+      const dob = formData.date_of_birth
+      if (dob){
+        const now = new Date();
+        return new Date(dob) >= new Date(now.getFullYear() - 13, now.getMonth(), now.getDate());
+      }
+  
+    }
+
+
+
+  const fullForm = data.map(e => { 
+    Tag = e.tag
+    const required =  e.required === "true" ? true :false
+
+
+  
+
+    return (
+        <>
+            
+            <label htmlFor={e.human_label}>{e.human_label}</label>
+            <Tag
+                key={e.name}
+                type={e.type}
+                placeholder={e.placeholder}
+                name={e.name}
+                required={required}
+                conditional={e.conditional}
+                checked={needsParentalConsent()}
+                value={data.name}
+                className={e.className}
+                onChange={handleChange}
+            />
+           
+        </>
+    )
+  })
+
+return (
+    <div className="form-container">
+        <h1>Personal Information</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          {fullForm} 
+          <button className="form--submit">Submit</button>
+        </form>
+        
     </div>
-  );
+)
+
 }
 
-export default App;
+
+
+export default App; 
+
+
+
